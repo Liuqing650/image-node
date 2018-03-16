fc41a747fa28d0ef77c18a888d46c129f5335321
qing.liu <qing.liu@socialcredits.cn>
2018-03-16 16:47:03 +0800

    测试图片和数据

--- image.js
+++ image.js
@@ -1,38 +1,62 @@
 var fs = require("fs");
 var images = require("images");
-// 异步读取
-fs.readFile('input.txt', function (err, data) {
-  if (err) {
-    return console.error(err);
+var ProgressBar = require('./progress-bar');
+var pb = new ProgressBar('合并进度', 100);
+var position = [];
+var imageInfo = {};
+var fileName = '图片.png';
+var quality = 100;
+
+// 执行函数
+function setUp() {
+  getPosition();
+};
+
+// 异步读取图片位置信息
+function getPosition() {
+  fs.readFile('./pic/position.json', function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    position = bufferToJSON(data);
+    // console.log("position: " + data.toString());
+    getImageInfo();
+  });
+}
+
+// 异步读取图片尺寸信息
+function getImageInfo() {
+  fs.readFile('./pic/imageInfo.json', function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    imageInfo = bufferToJSON(data);
+    if (imageInfo && position && imageInfo.width && position.length > 0) {
+      console.log('数据接收完成,开始合并图片...');
+      merageImage(position, imageInfo);
+      console.log();
+      console.log('合并完成, 文件名: %s', fileName);
+    }
+  });
+}
+
+// BufferToJSON
+function bufferToJSON(data) {
+  return JSON.parse(data.toString());
+}
+
+setUp();
+
+function merageImage(posArr, info) {
+  var scale = 1000;
+  images.setLimit(info.width + scale, info.height + scale)
+  var drawImage = images(info.width, info.height);
+  for (var index = 0; index < posArr.length; index++) {
+    const pos = posArr[index];
+    pb.render({ completed: index, total: posArr.length - 1 });
+    drawImage.draw(images(`./pic/pic${index}.png`), pos.x, pos.y);
   }
-  console.log("异步读取: " + data.toString());
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// images.setLimit(40000, 8000)
-// images(30000, 1000)                     //Load image from file 
-//   //加载图像文件
-//   // .size(4000)                          //Geometric scaling the image to 400 pixels width
-//   //等比缩放图像到400像素宽
-//   .draw(images("test.jpeg"), 10, 10)   //Drawn logo at coordinates (10,10)
-//   .draw(images("code.png"), 10, 1000)   //Drawn logo at coordinates (10,10)
-//   //在(10,10)处绘制Logo
-//   .save("output.png", {               //Save the image to a file,whih quality 50
-//     quality: 100                    //保存图片到文件,图片质量为50
-//   });
+  drawImage.save(fileName, { 
+    quality: quality
+  });
+}
